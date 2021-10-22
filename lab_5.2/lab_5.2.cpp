46fe1020a201189f9584343a00356c9671180f53
ArtemSadovenko <90398799+ArtemSadovenko@users.noreply.github.com>
2021-10-22 13:24:58 +0300

    Add files via upload

--- lab_5.2/lab_5.2.cpp
+++ lab_5.2/lab_5.2.cpp
@@ -1,50 +1,56 @@
-﻿#include <iostream>
-#include <iomanip>
-#include <cmath> 
+﻿#include<iostream>
+#include<iomanip>
+#include<cmath>
 
 using namespace std;
 
-void S(const double x, const double eps, int& n, double& s); 
-void A(const double x, const int n, double& a);
+double S(const double x, const double eps, int& n, double s);
+double A(const double x, const int n, double a);
 
-int main() {
-    double xp, xk, x, dx, eps, s = 0;     
-    int n = 0;
+int main()
+{
+	double xp, xk, x, dx, eps, s = 0;
+	int n = 0;
 
-    cout << "xp = "; cin >> xp;     
-    cout << "xk = "; cin >> xk;     
-    cout << "dx = "; cin >> dx;    
-    cout << "eps = "; cin >> eps;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
 
-    cout << fixed;         
-    cout << "-------------------------------------------------" << endl;         
-    cout << "|" << setw(5) << "x" << "     |" << setw(10) << "exp(x)" << "   |" << setw(7) << "S" << "      |" << setw(5) << "n" << "   |" << endl;         
-    cout << "-------------------------------------------------" << endl;     
-    x = xp;    
-    while (x <= xk) {
-        S(x, eps, n, s);
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << "     |" << setw(10) << "exp(x)" << "   |" << setw(7) << "S" << "      |" << setw(5) << "n" << "   |" << endl;
+	cout << "-------------------------------------------------" << endl;
 
-        cout << "|" << setw(7) << setprecision(2) << x << "   |" << setw(10) << setprecision(5) << log(x) << "   |" << setw(10) << setprecision(5) << s << "   |" << setw(5) << n << "   |" << endl;         
-        x += dx;
-    }         
-    cout << "-------------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		s = S(x, eps, n, s);
 
-    return 0;
+		cout << "|" << setw(7) << setprecision(2) << x << "   |" << setw(10) << setprecision(5) << log(x) << "   |" << setw(10) << setprecision(5) << s << "   |" << setw(5) << n << "   |" << endl << "-------------------------------------------------" << endl;
+
+		x += dx;
+	}
+
+	return 0;
 }
 
-void S(const double x, const double eps, int& n, double& s) {
-    n = 0;                   
-    double a = x - 1;             
-    s = a;     
-    do{         
-        n++;         
-        A(x, n, a);         
-        s += a;     
-    }while (abs(a)>=eps); 
-} 
-    
+double S(const double x, const double eps, int& n, double s)
+{
+	n = 0;
+	double a = x - 1;
+	s = a;
+	do {
+		n++;
+		a = A(x, n, a);
+		s += a;
+	} while (abs(a) >= eps);
+	return s;
+}
 
-void A(const double x, const int n, double& a) {
-    double r = -(n * (x - 1.0)) / (n + 1.0);
-    a *= r; 
+double A(const double x, const int n, double a)
+{
+	double r = -(n * (x - 1.0)) / (n + 1.0);
+	a *= r;
+	return a;
 }
